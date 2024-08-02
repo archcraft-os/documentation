f8f0518da9a4deee987766b39ae9d242fa1e2729
positive-season <scp94373@gmail.com>
2024-08-02 20:01:13 +0100

    Fixed typo, should end with '/'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -208,7 +208,7 @@ const config = {
       // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
       replaceSearchResultPathname: {
         from: '/docs/', // or as RegExp: /\/docs\//
-        to: '/docs',
+        to: '/docs/',
       },
 
       // Optional: Algolia search parameters
